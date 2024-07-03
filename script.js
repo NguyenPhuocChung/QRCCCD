8f191902d3e1cc663f05a5a894297a670148709a
NguyenPhuocChung <137128448+NguyenPhuocChung@users.noreply.github.com>
2024-07-03 17:18:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function formatDate(dateString) {
 // Tạo một instance của trình quét. 'reader' là ID của phần tử HTML.
 let html5QrcodeScanner = new Html5QrcodeScanner("reader", {
   fps: 15,
-  qrbox: 50,
+  qrbox: 400,
 });
 
 // Bắt đầu quét.
